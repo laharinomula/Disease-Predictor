ac334763fd30c0b12d3d9a35f89dcba72cced019
laharinomula <135495849+laharinomula@users.noreply.github.com>
2024-01-13 20:04:16 +0530

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split, cross_val_score
+
+from sklearn.ensemble import RandomForestClassifier
+
+
+
+
+data=pd.read_csv("Training.csv").dropna(axis = 1)
+print(data)
+encoder = LabelEncoder()
+data["prognosis"] = encoder.fit_transform(data["prognosis"])
+X = data.iloc[:,:-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test =train_test_split(X, y, test_size = 0.2, random_state = 24)
+rf_model = RandomForestClassifier(random_state=18)
+rf=rf_model.fit(X_train, y_train)
+    
+pickle.dump(rf,open('model.pkl','wb'))
+
+
